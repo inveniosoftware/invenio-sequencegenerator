3ec6f5b4ce9d64d7821f58273326781aeda60290
Invenio <info@inveniosoftware.org>
2018-11-26 16:38:05 -0500

    global: license change and files cleanup

--- examples/app.py
+++ examples/app.py
@@ -1,26 +1,10 @@
 # -*- coding: utf-8 -*-
 #
 # This file is part of Invenio.
-# Copyright (C) 2016 CERN.
+# Copyright (C) 2016-2018 CERN.
 #
-# Invenio is free software; you can redistribute it
-# and/or modify it under the terms of the GNU General Public License as
-# published by the Free Software Foundation; either version 2 of the
-# License, or (at your option) any later version.
-#
-# Invenio is distributed in the hope that it will be
-# useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-# General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with Invenio; if not, write to the
-# Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,
-# MA 02111-1307, USA.
-#
-# In applying this license, CERN does not
-# waive the privileges and immunities granted to it by virtue of its status
-# as an Intergovernmental Organization or submit itself to any jurisdiction.
+# Invenio is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
 
 
 """Minimal Flask application example for development.
@@ -45,6 +29,7 @@ from flask import Flask
 from flask_admin import Admin
 from flask_babelex import Babel
 from invenio_db import InvenioDB, db
+
 from invenio_sequencegenerator.admin import counter_adminview as ca
 from invenio_sequencegenerator.admin import templatedefinition_adminview as ta
 from invenio_sequencegenerator.api import Sequence, Template
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,26 +1,10 @@
 # -*- coding: utf-8 -*-
 #
 # This file is part of Invenio.
-# Copyright (C) 2015, 2016 CERN.
+# Copyright (C) 2015-2018 CERN.
 #
-# Invenio is free software; you can redistribute it
-# and/or modify it under the terms of the GNU General Public License as
-# published by the Free Software Foundation; either version 2 of the
-# License, or (at your option) any later version.
-#
-# Invenio is distributed in the hope that it will be
-# useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-# General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with Invenio; if not, write to the
-# Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,
-# MA 02111-1307, USA.
-#
-# In applying this license, CERN does not
-# waive the privileges and immunities granted to it by virtue of its status
-# as an Intergovernmental Organization or submit itself to any jurisdiction.
+# Invenio is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
 
 
 """Pytest configuration."""
@@ -34,10 +18,11 @@ import tempfile
 import pytest
 from flask import Flask
 from flask.cli import ScriptInfo
-from invenio_db import db as db_
 from invenio_db import InvenioDB
-from invenio_sequencegenerator.ext import InvenioSequenceGenerator
+from invenio_db import db as db_
 from sqlalchemy_utils.functions import create_database, database_exists
+
+from invenio_sequencegenerator.ext import InvenioSequenceGenerator
 
 
 @pytest.yield_fixture()
